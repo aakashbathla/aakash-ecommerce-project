f880b134050068359e241dc886b550597d2e99e1
aakashbathla <aakash.bathla7@gmail.com>
2022-03-08 20:05:11 +0400

    code udpated

--- src/App.js
+++ src/App.js
@@ -12,16 +12,24 @@ export default function App() {
   }, []);
   return (
     <div className="App">
-      {products.map((val) => (
-        <div class="card bg-light mb-3">
-          <div class="card-body">
-            <img alt="product" class="img-fluid image-style" src={val.image} />
-            <h5 class="card-title">{val.title}</h5>
-            <p class="card-text">{val.description}</p>
-            <p class="bloc_left_price">{val.price}</p>
+      <div className="product-container">
+        {products.map((val) => (
+          <div className="card bg-light mb-3">
+            <div className="card-body">
+              <img
+                alt="product"
+                className="img-fluid image-style"
+                src={val.image}
+              />
+              <h5 className="card-title">{val.title}</h5>
+              <p className="card-text">{val.description}</p>
+              <p className="bloc_left_price">{val.price}</p>
+              <button className="addToCart">Add to cart</button>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
+      <div className="cart-container">Cart Listing Items</div>
     </div>
   );
 }
